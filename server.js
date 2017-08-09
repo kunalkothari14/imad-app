d74b21159149b7bfa212e902c1ebbbd2ad3de76d
kunalkothari14 <kotharikunal1996@gmail.com>
2017-08-09 20:10:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName',function(req,res){
+app.get('/:articleName',function(req,res){
   // article name--article-one
   // articles[articleName]--{}content object for article -one
 var articleName = req.params.articleName;
