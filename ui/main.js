96ead625934f4148e0b6a59559ed4ad54f838d80
kunalkothari14 <kotharikunal1996@gmail.com>
2017-08-10 03:27:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,15 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 
 element.innerHTML="New Value";
+
+var img=document.getElementById("madi");
+var marginLeft=0;
+
+function moveright(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+    
+}
+img.onclick=function(){
+    var interval=setInterval(moveright,50);
+};
