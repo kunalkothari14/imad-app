3720d3b5fe556b469ec9a4454e1a0c4664216cbc
kunalkothari14 <kotharikunal1996@gmail.com>
2017-08-12 03:28:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,16 +15,21 @@ img.onclick=function(){
 
 var button=document.getElementById('counter');
 button.onclick=function(){
+    //create a request 
     var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readystate==XMLHttpRequest.DONE){
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function () {
+        if(request.readyState===XMLHttpRequest.DONE){
             //take some action
-            if(request.status==200){var counter=request.response.Text;
+            if(request.status===200){
+                var counter=request.responseText;
             var span=document.getElementById('count');
             span.innerHTML=counter.toString();
              }
         }
     };
+    //MAKE A REQUEST TO THE COUNTER END POINT
     request.open('GET','http://kotharikunal1996.imad.hasura-app.io/counter',true);
     request.send(null);
 };
