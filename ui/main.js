7c298c6c33cb2b3b1d67aa5d4f4b1d6d071b108f
kunalkothari14 <kotharikunal1996@gmail.com>
2017-08-12 03:17:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,5 @@
 console.log('Loaded!');
 //change the text of the main-text div
-var element=document.getElementById("main-text");
-
-element.innerHTML="New Value";
 
 var img=document.getElementById("madi");
 var marginLeft=0;
@@ -15,3 +12,40 @@ function moveright(){
 img.onclick=function(){
     var interval=setInterval(moveright,10);
 };
+
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate==XMLHttpRequest.DONE){
+            //take some action
+            if(request.status==200){var counter=request.response.Text;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+             }
+        }
+    };
+    request.open('GET','http://kotharikunal1996.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
