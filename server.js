0e6b71397736b0df3722f350a0725c6c9658bae4
kunalkothari14 <kotharikunal1996@gmail.com>
2017-08-12 03:22:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,10 +80,16 @@ var htmlTemplate=`<!DOCTYPE html>
   return htmlTemplate;
 }
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
+
 
 app.get('/:articleName',function(req,res){
   // article name--article-one
