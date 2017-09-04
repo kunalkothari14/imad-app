81a780a19f31d2be45024f160e1f1e947f922e05
kunalkothari14 <kotharikunal1996@gmail.com>
2017-09-04 23:15:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,11 +93,11 @@ var names=[];
 });
 
 
-var pool=new Pool();
+
 app.get('/articles/:articleName',function(req,res){
   // article name--article-one
   // articles[articleName]--{}content object for article -one
-   pool.query('SELECT * FROM  articless WHERE title =$1',[req.params.articleName],function(err,result)
+   pool.query("SELECT * FROM  articless WHERE title =$1",[req.params.articleName],function(err,result)
     {
         if (err){
             res.status(500).send(err.toString());
